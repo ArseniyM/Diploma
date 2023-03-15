6d2d24263527a53e900cc8871d72a1cac5f80325
AverkinaNastya <Vitanikini14@mail.ru>
2023-03-15 23:54:01 +0300

    Добавление свойств Заголовок окна и Цвет фона окна для AuthorizationWindow

--- ProjectManagement/ViewModels/AuthorizationWindowViewModel.cs
+++ ProjectManagement/ViewModels/AuthorizationWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,24 @@ namespace ProjectManagement.ViewModels
 {
     class AuthorizationWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Авторизация";
 
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+        #region Цвет фона окна
+        private string _Background = "#46486C";
+
+        public string Background
+        {
+            get => _Background;
+            set => Set(ref _Background, value);
+        }
+        #endregion
     }
 }
