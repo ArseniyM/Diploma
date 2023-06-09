4a924f6bb239001a974128c2928aef7dca156dc4
AverkinaNastya <vitanikini14@mail.ru>
2023-06-09 04:25:29 +0300

    Исправлено название кнопки на форме изменения проекта

--- ProjectManagement/obj/Debug/net7.0-windows/Views/UserControls/LeftMenuBarUI.g.i.cs
+++ ProjectManagement/obj/Debug/net7.0-windows/Views/UserControls/LeftMenuBarUI.g.i.cs
@@ -57,7 +57,7 @@ namespace ProjectManagement.Views.UserControls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ProjectManagement;component/views/usercontrols/leftmenubarui.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProjectManagement;V1.0.0.0;component/views/usercontrols/leftmenubarui.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\UserControls\LeftMenuBarUI.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
